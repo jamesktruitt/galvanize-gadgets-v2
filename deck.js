9bf90383a86646c385cc04c01bb29c4fb6a0147a
James Truitt <tru@rockyagirl.com>
2018-06-09 16:15:57 -0600

    all tests passing

---
+++ deck.js
@@ -0,0 +1,33 @@
+class Card {
+  constructor(rank, suit) {
+    this.rank = rank
+    this.suit = suit
+  }
+}
+
+class Deck {
+  constructor() {
+    this.cards = []
+  }
+
+  add(card) {
+    this.cards.push(card)
+  }
+
+  filter(suit) {
+    return this.cards.filter(current => {
+      return current.suit === suit 
+    })
+  }
+  populate(array) {
+    array.forEach(current => {
+      return this.cards.push(new Card(current[0], current[1]))
+    })
+    return this.cards
+  }
+}
+
+module.exports = {
+  Card,
+  Deck
+};
